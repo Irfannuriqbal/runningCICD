385db48a0bf157829164aa5908e677de88bab8b8
Irfan Nur Iqbal <irfannuriqbal19@gmail.com>
2026-03-13 16:10:37 +0700

    Initial commit - CI/CD Project

---
+++ script.js
@@ -0,0 +1,5 @@
+function showMessage() {
+  const box = document.getElementById("message");
+  box.classList.remove("hidden");
+  box.innerHTML = "&#9989; Deployment successful &mdash; CI/CD pipeline is running perfectly!";
+}
