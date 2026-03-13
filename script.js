5c8784ebd2238eff94802fb558a145d098d43023
Irfan Nur Iqbal <irfannuriqbal19@gmail.com>
2026-03-13 16:57:18 +0700

    Test final

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function showMessage() {
   const box = document.getElementById("message");
   box.classList.remove("hidden");
-  box.innerHTML = "&#9989; Deployment successful &mdash; CI/CD pipeline is running perfectly!";
+  box.innerHTML =
+    "&#9989; Deployment successful &mdash; CI/CD pipeline is running perfectly!";
 }
